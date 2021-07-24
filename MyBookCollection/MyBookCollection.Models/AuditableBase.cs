d8057fd4cea218197ad253699340c7b59bc16f02
Sawarni <33835720+Sawarni@users.noreply.github.com>
2021-07-24 14:44:59 +0530

    Make build pass again

--- MyBookCollection/MyBookCollection.Models/AuditableBase.cs
+++ MyBookCollection/MyBookCollection.Models/AuditableBase.cs
@@ -6,7 +6,7 @@ namespace MyBookCollection.Models
 {
     public class AuditableBase
     {
-        public DateTim CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
 
         public DateTime? UpdatedDate { get; set; }
 
